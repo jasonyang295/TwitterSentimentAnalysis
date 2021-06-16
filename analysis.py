b78a06603104a6e251e8277828f331f805e2c76b
Jason Yang <canyoncrestjy@gmail.com>
2021-06-15 20:15:03 -0700

    Delete analysis.py

--- analysis.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot
-import seaborn as sns
-import warnings
-warnings.filterwarnings('ignore')
-
-#load dataset
-df = pd.read_csv("data/covid19_tweets.csv")
